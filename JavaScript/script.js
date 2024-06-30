7a81665644854eea30464f4e59e5c07d6fe1358b
Zoc Nishant <zocnishant@gmail.com>
2024-06-30 17:12:55 +0545

    JS

---
+++ JavaScript/script.js
@@ -0,0 +1,29 @@
+// js the language
+// basics
+// inbuilt features
+
+// console.log("Yes Pasa I am back and will stick with you.");
+// console.warn("Dude k vo");
+// console.error("You got an error dude.");
+
+// alert("Nepal is the best country.");
+// prompt("What up dude?");
+
+// variables constants
+// let a = 12;
+// const pi = 3.14;
+
+// compilers and interoreters
+
+// window
+// type
+//conditionals
+// loops
+// functions
+// arrays
+// objects
+// prototypes
+// prototypal inheritance
+// asynchronous
+// es6 climax
+// questions
