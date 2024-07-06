e8cf2e1a3a9a68e95248710e4c4160d9705deb36
Zoc Nishant <zocnishant@gmail.com>
2024-07-06 16:56:00 +0545

    DOM

---
+++ JavaScript/DOM/scriptDom.js
@@ -0,0 +1,28 @@
+// DOM
+
+// DOM Manipulation
+
+// Accessing ElementInternals:
+// document.querySelector()
+
+// Modifying Elements:
+// innerHTML, textContent
+
+// Manipulating Styles and Classes:
+// style
+// classList
+
+// Creating and Deleting Elements:
+// createElement()
+// appendChild()
+// removeChild()
+
+// Event Handling
+// addEventListener()
+
+// Even Object
+
+// ACCESSING ELEMSNTS
+const btn = document.querySelector("button");
+
+btn.innerHTML = "<h1>Pasa</h1>";
